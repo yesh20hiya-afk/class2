48b6d3fec06d0eeb551ff766927976a3c0800bcd
yesh20hiya-afk <yesh20hiya@gmail.com>
2025-12-10 20:17:13 -0500

    v1

---
+++ Activity1.py
@@ -0,0 +1,6 @@
+print("My name is Yesh" , "I am in fifth grade", end = " ")
+print("My name is Yesh " , "I am in fifth grade" , sep="-" )
+print(30)
+print(30+20)
+print("""Hey there,
+      Welcome to Python programming!""")
